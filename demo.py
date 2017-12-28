613cc4c176eda4fda0d2df65748981441721a5b7
ayushoriginal <ayush.original@gmail.com>
2017-12-28 18:31:06 +0530

    add loss function

--- demo.py
+++ demo.py
@@ -1,11 +1,25 @@
+# @uthor and Intructor - Ayush Pareek
+# Gradient Descent Tutorial - Deep Learning Series - CybrosX Lectures
 from numpy import *
 
+def compute_errror_for_given_points(b,m,points):
+	totalError = 0
+	for i in range(0,len(points)):
+		x = points[i,0]
+		y = points[0,1]
+		totalError += (y-(m*x+b)) ** 2
+	return totalError
+
+
+def step_gradient (b_current, m_current,)
 
 def gradient_descent_runner(points, initial_b, initial_m, learning_rate ,num_iterations):
 	b = starting_b
 	m=  starting_m
 
 	for i in range(num_iterations):
+		b, m = step_gradient (b,m, array(b,m), learning_rate)
+	return [b,m]
 		
 
 def run():
@@ -19,8 +33,6 @@ def run():
 	[b,m]=gradient_descent_runner(points, initial_b, initial_m, learning_rate ,num_iterations)
 	print b
 	print m
-
-
 
 
 
